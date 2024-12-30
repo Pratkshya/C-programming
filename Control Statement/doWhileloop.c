23c71d6fca5d2ddc657ebe28627c97dcb3e4c03d
Pratkshya <pratikshyaparajuli37@gmail.com>
2024-12-30 16:15:03 +0000

    do while loop

---
+++ Control Statement/doWhileloop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+  int i;
+  i = 1;
+  do
+  {
+    printf("%d", i);
+    printf("\n");
+    i++;
+  } while (i <= 5);
+  
+}
---
+++ Control Statement/forLoop.c
@@ -0,0 +1,12 @@
+//To count the numbers from 1 - 10
+
+#include <stdio.h>
+void main()
+{
+  int i;
+  for (i = 1; i<=10; i++)
+  {
+    printf("%d", i);
+    printf("\n");
+  }
+}
